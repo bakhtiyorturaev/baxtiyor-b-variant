e3bbc2291f04937d273f30b54ede15b1217028d5
bakhtiyorturaev <bakhtiyor6364@gmail.com>
2024-01-04 18:46:02 +0500

    masala_1

---
+++ masala_1.py
@@ -0,0 +1,15 @@
+def faktorial_n_dan_m_gacha(n, m):
+    result = 1
+    for i in range(n, m+1):
+        result *= i
+    count = 0
+    while result % 10 == 0:
+        count += 1
+        result //= 10
+    return count
+
+n = int(input("n ni kiriting: "))
+m = int(input("m ni kiriting: "))
+
+nollar_soni = faktorial_n_dan_m_gacha(n, m)
+print(f"{n} dan {m} gacha bo'lgan sonlar ko'paytmasidagi oxirida hosil bo'lgan nollar soni: {nollar_soni}")
