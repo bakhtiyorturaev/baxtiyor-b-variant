1f51ca74152afab41cea14ed2fc69f2568206f06
bakhtiyorturaev <bakhtiyor6364@gmail.com>
2024-01-04 19:47:35 +0500

    masala_2

--- masala_4.py
+++ masala_4.py
@@ -1,22 +1,16 @@
-def dec_func(origin_func):
-    def wrapper(*args, **kwargs):
-        val = origin_func(*args, **kwargs)
-        for i in val:
-            a = []
-            for j in i:
-                if i % j == 0:
-                    a += 1
-                    if a == 2:
-                        a.append(i)
-                        print(a)
+def tub_sonlarni_tartiblash(f):
+    def wrapper(nums):
+        sonlar = [num for num in nums if isinstance(num, int) and num > 1 and all(num % i != 0 for i in range(2, int(num ** 0.5) + 1))]
+        sorted_sonlar = sorted(sonlar)
+        print(f(sorted_sonlar))
 
     return wrapper
 
 
-@dec_func
+@tub_sonlarni_tartiblash
 def lists(a):
     return a
 
 
-A = ['a', 1, 2, 'sd',11]
+A = ['a', 1, 2, 'sd', 11, 17, 21, 'fhjaf', 23, 25]
 lists(A)
