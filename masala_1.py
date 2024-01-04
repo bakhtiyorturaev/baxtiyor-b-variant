214dd07098bd4204aad79862f5f86afbe5822338
bakhtiyorturaev <bakhtiyor6364@gmail.com>
2024-01-04 18:53:20 +0500

    masala_1.

--- masala_1.py
+++ masala_1.py
@@ -1,12 +1,13 @@
 def faktorial_n_dan_m_gacha(n, m):
     result = 1
-    for i in range(n, m+1):
+    for i in range(n, m + 1):
         result *= i
     count = 0
     while result % 10 == 0:
         count += 1
         result //= 10
     return count
+
 
 n = int(input("n ni kiriting: "))
 m = int(input("m ni kiriting: "))
